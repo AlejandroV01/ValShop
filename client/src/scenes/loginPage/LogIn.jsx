23a42acf1d48f25b8a4e7bd1f1d03436144e88a1
Alejandro <alexvera0109@gmail.com>
2023-05-09 02:50:26 -0400

    further progress on login and signup

---
+++ client/src/scenes/loginPage/LogIn.jsx
@@ -0,0 +1,78 @@
+import { Email, Key, RememberMe, Visibility, VisibilityOff } from '@mui/icons-material'
+import { Box, Button, FormControl, Icon, IconButton, InputAdornment, InputLabel, Link, OutlinedInput, Typography, useTheme } from '@mui/material'
+import FlexBetween from 'components/FlexBetween'
+import React, { useState } from 'react'
+const LogIn = ({ toggleLoginPage }) => {
+  const theme = useTheme()
+  const neutralLight = theme.palette.neutral.light
+  const dark = theme.palette.neutral.dark
+  const background = theme.palette.background.default
+  const primaryLight = theme.palette.primary.light
+  const alt = theme.palette.background.alt
+  const [showPassword, setShowPassword] = useState(false)
+
+  return (
+    <>
+      <Box display='flex' flexDirection='column' alignItems='flex-start'>
+        <Typography variant='h5' color={theme.palette.neutral.mediumMain}>
+          WELCOME BACK
+        </Typography>
+        <Typography variant='h1' margin='1rem 0'>
+          Sign In.
+        </Typography>
+        <Typography variant='h5' color={theme.palette.neutral.mediumMain} marginBottom='2rem'>
+          Don't have an account?{' '}
+          <Link
+            underline='none'
+            color='primary'
+            sx={{
+              '&:hover': {
+                cursor: 'pointer',
+              },
+            }}
+            onClick={toggleLoginPage}
+          >
+            Sign Up
+          </Link>
+        </Typography>
+      </Box>
+      <Box display='flex' flexDirection='column' gap='0.75rem'>
+        <FormControl>
+          <InputLabel>Email</InputLabel>
+          <OutlinedInput
+            type='email'
+            endAdornment={
+              <InputAdornment position='end'>
+                <Icon edge='end'>
+                  <Email />
+                </Icon>
+              </InputAdornment>
+            }
+            label='Email'
+          ></OutlinedInput>
+        </FormControl>
+        <FormControl>
+          <InputLabel>Password</InputLabel>
+          <OutlinedInput
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position='end'>
+                <IconButton edge='end' onClick={() => setShowPassword(!showPassword)}>
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label='Password'
+          ></OutlinedInput>
+        </FormControl>
+        <Box>
+          <Button variant='contained' size='large'>
+            Login
+          </Button>
+        </Box>
+      </Box>
+    </>
+  )
+}
+
+export default LogIn
